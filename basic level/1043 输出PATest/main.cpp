6402c82f65161a43c7369caf8514f2b34af9628b
xiaojiang0318 <1339675740@qq.com>
2019-07-02 10:13:33 +0800

    test 9

--- basic level/1043 输出PATest/main.cpp
+++ basic level/1043 输出PATest/main.cpp
@@ -6,53 +6,63 @@
 //Copyright  2019年 Robert. All rights reserved.
 //
 
-
 #include <iostream>
 
 using namespace std;
 int main(){
     char a[10001];
-    cin.getline(a,10001);
-    int i=0,P=0,A=0,T=0,e=0,s=0,t=0;
-    while(a[i]!='\0'){
-        switch (a[i++])
-        {
+    cin.getline(a, 10001);
+    int i = 0, P = 0, A = 0, T = 0, e = 0, s = 0, t = 0;
+    while (a[i] != '\0'){
+        switch (a[i++]){
         case 'P':
             P++;
             break;
         case 'A':
             A++;
-            break; 
+            break;
         case 'T':
             T++;
-            break; 
+            break;
         case 'e':
             e++;
-            break; 
+            break;
         case 's':
             s++;
-            break; 
+            break;
         case 't':
             t++;
-            break; 
+            break;
         default:
             break;
         }
     }
-   
-   while(P||A||T||e||s||t){
-        if(P){
-            putchar('P');P--;}
-        if(A){
-            putchar('A');A--;}
-        if(T){
-            putchar('T');T--;}
-        if(e){
-            putchar('e');e--;}
-        if(s){
-            putchar('s');s--;}
-        if(t){
-            putchar('t');t--;}
-   }
+
+    while (P || A || T || e || s || t){
+        if (P){
+            putchar('P');
+            P--;
+        }
+        if (A){
+            putchar('A');
+            A--;
+        }
+        if (T){
+            putchar('T');
+            T--;
+        }
+        if (e){
+            putchar('e');
+            e--;
+        }
+        if (s){
+            putchar('s');
+            s--;
+        }
+        if (t){
+            putchar('t');
+            t--;
+        }
+    }
     return 0;
 }
