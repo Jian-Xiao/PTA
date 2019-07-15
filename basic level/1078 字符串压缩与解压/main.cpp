5977ab89880c61998cf02c369a6af399812c3d36
xiaojiang0318 <1339675740@qq.com>
2019-07-15 21:38:40 +0800

    printf \n代表多个空字符串

---
+++ basic level/1078 字符串压缩与解压/main.cpp
@@ -0,0 +1,88 @@
+//
+//main.cpp
+//index-name
+//
+//Created by Robert on 2019/3/26.
+//Copyright  2019年 Robert. All rights reserved.
+//
+
+
+#include <iostream>
+
+using namespace std;
+char qus[1001];
+char ans[100000];
+int start=0;
+void compress(){
+    char a,b;
+    int num=1,index=0;
+    b=qus[start++];
+    while(b!='\0'){
+        a=b;
+        b=qus[start++];
+        if(a==b){
+            num++;
+        }
+        else{
+            if(num==1){
+                ans[index++]=a;
+            }
+            else{
+                if(num>=1000){
+                    ans[index++]=num/1000+'0';
+                    ans[index++]=(num/100)%10+'0';
+                    ans[index++]=(num/10)%10+'0';
+                    ans[index++]=num%10+'0';
+                }
+                else if(num>=100){
+                    ans[index++]=num/100+'0';
+                    ans[index++]=(num/10)%10+'0';
+                    ans[index++]=num%10+'0';
+                }
+                else if(num>=10){
+                    ans[index++]=num/10+'0';
+                    ans[index++]=num%10+'0';
+                }
+                else{
+                    ans[index++]=num+'0';
+                } 
+                ans[index++]=a;
+                num=1;
+            }
+        }
+    }
+    ans[index]='\0';
+}
+void deCompress(){
+    char a,b;
+    int num=0,index=0;
+    b=qus[start++];
+    while(b!='\0'){
+        a=b;
+        b=qus[start++];
+        if(a<='9'&&a>='0'){
+            num=num*10+a-'0';
+        }
+        else{
+            num=num==0?1:num;
+            fill(ans+index,ans+index+num,a);
+            index+=num;
+            num=0;
+        }
+    }
+    ans[index]='\0';
+}
+int main(){
+    char c;
+    scanf("%c",&c);
+    getchar();
+    cin.getline(qus,1001);
+    if(c=='C'){
+        compress();
+    }
+    else{
+        deCompress();
+    }
+    printf("%s\n",ans);
+    return 0;
+}
